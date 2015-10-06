5a39515677c7550897f22af73999b3f98e798b94
chenb0x <artofmorellc@gmail.com>
2015-10-06 14:27:57 -0700

    Deck Selection, still needs debugging

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ blackjack-trainer
 by MasterChen @chenb0x
 ----------------------
 
-### To Do Items 10/02/2015
+### To Do Items 10/06/2015
 
 1. Single, Double, and Six deck trials
+	-> Counts work with single, double, and six deck selections, but...
+	-> Selection breaks after first round of play. Will debug soon.
 2. Running and true count calculations
--- bjtrainer.py
+++ bjtrainer.py
@@ -41,24 +41,59 @@ def counter(card):
 def usage():
 	print "Welcome to the BlackJack Trainer (Super Alpha)\n\n"
 
+# Deck selection
+def deckselect():
+	d = 0
+	print d
+	while d not in [1,2,6]:
+		d = input("How many decks are we playing with? (1, 2, or 6)")
+		d = int(d)
+		if d == 1:
+			print "Single Deck.\n"
+			d = 1
+			return d
+		elif d == 2:
+			d = 2
+			print "Double Deck.\n"
+			return d
+		elif d == 6:
+			print "6 Deck Shoe.\n"
+			d = 6
+			return d
+		else:
+			print "You didn't choose a proper deck size."
+			d = 0
+	return d
+
 # More initialization
-d1 = Deck()
-d2 = Deck()
-fulldeck = d1 + d2
+fulldeck = []
+decks = deckselect()
+print decks
+while decks > 0:
+	tmp = Deck()
+	fulldeck = fulldeck + tmp
+	decks = decks - 1
 print fulldeck
+
 count = 0
 exit = False
 
 while not exit:
 	try:
 		card1 = fulldeck[random.randint(0,len(fulldeck)-1)]
-		card2 = fulldeck[random.randint(0,len(fulldeck)-1)]
 		fulldeck.remove(card1)
+		card2 = fulldeck[random.randint(0,len(fulldeck)-1)]
 		fulldeck.remove(card2)
 	except ValueError:
-		d1 = Deck()
-		d2 = Deck()
-		fulldeck = d1 + d2
+		count = 0
+		fulldeck = []
+		decks = deckselect()
+		print decks
+		while decks > 0:
+        		tmp = Deck()
+        		fulldeck = fulldeck + tmp
+        		decks = decks - 1
+		print fulldeck
 		card1 = fulldeck[random.randint(0,len(fulldeck)-1)]
 		fulldeck.remove(card1)
 		card2 = fulldeck[random.randint(0,len(fulldeck)-1)]
