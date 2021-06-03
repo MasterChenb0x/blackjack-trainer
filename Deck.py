ecf3ff0982c3f1c8576d62015b57e5b216c28c17
MasterChenb0x <artofmorellc@gmail.com>
2021-06-03 19:38:41 -0400

    Restarted Deck construction

---
+++ Deck.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import random
+
+class Deck:
+    def __init__(self):
+        self.cards = {}
+        self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
+
+        for r in self.ranks:
+            self.cards[r] = {'suits': ['diamonds', 'clubs', 'hearts', 'spades']
+            }
+        if r in ['2', '3', '4', '5', '6']:
+            self.cards[r]['count_val'] = 1
+            self.cards[r]['num_val'] = int(r)
+        if r in ['7', '8', '9']:
+            self.cards[r]['count_val'] = 0
+        if r in ['10', 'jack', 'queen', 'king']:
+            self.cards[r]['count_val'] = -1
+            self.cards[r]['num_val'] = 10
+        if r in ['ace']:
+            self.cards[r]['count_val'] = -1
+            self.cards[r]['num_val'] = [1, 11]
+
+
+new_deck = Deck()
+print(new_deck.cards)
