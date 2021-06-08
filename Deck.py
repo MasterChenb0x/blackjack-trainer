6ec2176700e6c49a1c97d4f1a3a6085e020ba44f
chen <chen@kalaboratory.kalaboratory>
2021-06-08 17:10:19 -0400

    Fixed deck construction issue

--- Deck.py
+++ Deck.py
@@ -10,18 +10,18 @@ class Deck:
         for r in self.ranks:
             self.cards[r] = {'suits': ['diamonds', 'clubs', 'hearts', 'spades']
             }
-        if r in ['2', '3', '4', '5', '6']:
-            self.cards[r]['count_val'] = 1
-            self.cards[r]['num_val'] = int(r)
-        if r in ['7', '8', '9']:
-            self.cards[r]['count_val'] = 0
-            self.cards[r]['num_val'] = int(r)
-        if r in ['10', 'jack', 'queen', 'king']:
-            self.cards[r]['count_val'] = -1
-            self.cards[r]['num_val'] = 10
-        if r in ['ace']:
-            self.cards[r]['count_val'] = -1
-            self.cards[r]['num_val'] = [1, 11]
+            if r in ['2', '3', '4', '5', '6']:
+                self.cards[r]['count_val'] = 1
+                self.cards[r]['num_val'] = int(r)
+            if r in ['7', '8', '9']:
+                self.cards[r]['count_val'] = 0
+                self.cards[r]['num_val'] = int(r)
+            if r in ['10', 'jack', 'queen', 'king']:
+                self.cards[r]['count_val'] = -1
+                self.cards[r]['num_val'] = 10
+            if r in ['ace']:
+                self.cards[r]['count_val'] = -1
+                self.cards[r]['num_val'] = [1, 11]
 
     def deal(self):
         cards = list(self.cards.items())
