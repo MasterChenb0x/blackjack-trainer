381d3747535a5c3c1aebc27f451e38af26be91ab
chenb0x <artofmorellc@gmail.com>
2021-06-11 15:26:50 -0700

    Exception handling on countdown

--- blackjack_count_trainer.py
+++ blackjack_count_trainer.py
@@ -7,5 +7,10 @@ import random
 D1 = Deck()
 
 while D1:
-    print(D1.deal())
-    time.sleep(2)
+    #This loop should only run while there are cards on the deck, but it still throws an IndexError instead of exiting the loop.
+    #The break statement works, but is it the best approsch?
+    try:
+        print(D1.deal())
+        time.sleep(2)
+    except IndexError:
+        break
